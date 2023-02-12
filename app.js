543e2f145dc1ed9470716793ef9b1ca99a4e6e46
Your Name <psoyeon0127@naver.com>
2023-02-12 17:18:36 +0900

    ADD: routes/page.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const pageRouter = require('./routes');
+
 const app = express();
 app.set('port', process.env.PORT || 8001);
 app.set('view engine', 'html');
@@ -31,4 +33,21 @@ app.use(session({
     },
 }));
 
-app.use()
+app.use('/', pageRouter);
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url}  라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+})
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기중');
+});
