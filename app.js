c426c948b1eeba4646d70173da526f5e023330fb
Your Name <psoyeon0127@naver.com>
2023-03-06 20:46:35 +0900

    [ADD] passport

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
 const { sequelize } = require('./models');
 
 const app = express();
@@ -44,6 +45,7 @@ app.use(session({
 }));
 
 app.use('/', pageRouter);
+app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url}  라우터가 없습니다.`);
--- routes/auth.js
+++ routes/auth.js
@@ -4,3 +4,31 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 const router = express.Router();
+
+//회원가입 라우터
+router.post('/join', async(req, res, next) => {
+    const { email, nick, password } = req.body;
+    try {
+        const exUser = await User.findOne({ where: { email } });
+        if (exUser) {
+            return res.redirect('/join?error=exist');
+        }
+        const hash = await bcrypt.hash(password, 12);
+        await User.create({
+            email,
+            nick,
+            password: hash,
+        });
+        return res.redirect('/');
+    }
+    catch (error) {
+        console.error(error);
+        return next(error);
+    }
+});
+
+router.post('/login', () => {
+    
+});
+
+module.exports = router;
