dc7c7616175eecb5f21e02176792e4e5c8f3bc9a
Your Name <psoyeon0127@naver.com>
2023-03-06 15:22:28 +0900

    [FIX] index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'developement';
+const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const User = require('./user');
 const Post = require('./post');
