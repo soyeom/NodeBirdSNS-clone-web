3cc57d9f4c4951407c361f6ef7cb69cb7176bfec
Your Name <psoyeon0127@naver.com>
2023-03-10 21:18:05 +0900

    [ADD] localStrategy.js

---
+++ passport/localStrategy.js
@@ -0,0 +1,16 @@
+const passport= require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+
+module.exports = () => {
+    passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+    }, async(email, password, done) => {
+        try {
+            
+        }
+    }))
+}
