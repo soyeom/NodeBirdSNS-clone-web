d63d960ad8b891ebc9fc41716246d728edf5e1b2
Your Name <psoyeon0127@naver.com>
2023-04-04 22:44:10 +0900

    [ERROR] local login ERROR

--- passport/index.js
+++ passport/index.js
@@ -16,4 +16,5 @@ module.exports = () => {
     });
 
     local();
+    kakao();
 };
--- routes/page.js
+++ routes/page.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 
 const router = express.Router();
+
+router.use((req, res, next) => {
+    res.locals.user = req.user;
+    res.locals.followerCount = 0;
+    res.locals.followingCount = 0;
+    res.locals.followerList = [];
+    next();
+});
 
 router.get('/profile', (req, res) => {
     res.render('profile', { title: "내 정보 - NodeBird" });
