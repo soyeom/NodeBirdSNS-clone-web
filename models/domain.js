b03d402ed94efbe89beb16b4cf5e37590f77e6a5
Your Name <psoyeon0127@naver.com>
2023-04-14 15:26:46 +0900

    [ADD] api server

---
+++ models/domain.js
@@ -0,0 +1,31 @@
+/*
+const Sequelize = require('sequelize');
+
+module.exports = class Domain extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            host: { //nodecat의 url
+                type: Sequelize.STRING(80),
+                allowNull: false,
+            },
+            type: {
+                type: Sequelize.ENUM('free', 'premium'), //좀 더 상세한 문자열
+                allowNull: false,
+            },
+            clientSecret: { //키 발급
+                type: Sequelize.STRING(36),
+                allowNull: false,
+            },
+        }, {
+            sequelize,
+            timestamps: true,
+            paranoid: true,
+            modelName: 'Domain',
+            tableName: 'domains',
+        });
+    }
+    static associate(db) {
+        db.Domain.belongsTo(db.User);
+    }
+};
+*/
---
+++ routes/api.js
@@ -0,0 +1,23 @@
+/*
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const { User, Domain } = require('../models');
+const { isLoggedIn } = require('./middlewares');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        const user = await User.findOne({
+            where: { id: req.user && req.user.id || null },
+            include: { model: Domain },
+        });
+        res.render('login', {
+            user,
+            domains: user && user.Domains,
+        });
+    } catch(err) {
+        console.error(err);
+    }
+})
+*/
